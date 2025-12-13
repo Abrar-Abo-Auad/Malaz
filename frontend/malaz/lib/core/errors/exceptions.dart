4fb5baf0c6bde62095cd2af8c6a5d4c569a6b62a
LONYCODE <muhammadalhamwi963@gmail.com>
2025-12-14 01:30:08 +0300

    exceptions update

--- frontend/malaz/lib/core/errors/exceptions.dart
+++ frontend/malaz/lib/core/errors/exceptions.dart
@@ -1,6 +1,15 @@
-
 /// Represents errors that occur during data fetching or processing.
-class ServerException implements Exception {}
+class ServerException implements Exception {
+  final String message;
+  final int? statusCode;
+  final Map<String, dynamic>? errors;
+
+  ServerException({
+    required this.message,
+    this.statusCode,
+    this.errors,
+  });
+}
 
 /// Represents errors that occur when there is no internet connection.
 class NetworkException implements Exception {}
@@ -10,3 +19,9 @@ class CacheException implements Exception {}
 
 /// Can be used for other general exceptions.
 class GeneralException implements Exception {}
+
+class UnauthenticatedException implements Exception {
+  final String message;
+  UnauthenticatedException(
+      {this.message = 'Session expired, please login again'}); /// TODO: translate
+}
