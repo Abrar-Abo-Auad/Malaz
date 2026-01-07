01b90f5507c11406d382c10baf64c9b88876d9e8
LONYCODE <muhammadalhamwi963@gmail.com>
2026-01-07 20:18:21 +0300

    update translations files to support filter bottom sheet

--- frontend/malaz/lib/l10n/app_localizations_tr.dart
+++ frontend/malaz/lib/l10n/app_localizations_tr.dart
@@ -499,4 +499,61 @@ class AppLocalizationsTr extends AppLocalizations {
 
   @override
   String get back_to_top => 'Başa dön';
+
+  @override
+  String get filter_title => 'Sonuçları Filtrele';
+
+  @override
+  String get filter_reset => 'Sıfırla';
+
+  @override
+  String get filter_location_section => 'Konum';
+
+  @override
+  String get filter_current_location => 'Mevcut Konum';
+
+  @override
+  String get filter_map_location => 'Haritada';
+
+  @override
+  String get filter_price_section => 'Fiyat Aralığı (Dolar \$)';
+
+  @override
+  String get filter_type_section => 'Emlak Tipi';
+
+  @override
+  String get filter_area_section => 'Alan (m²)';
+
+  @override
+  String get filter_details_section => 'Özellikler (Min)';
+
+  @override
+  String get filter_total_rooms => 'Toplam Oda';
+
+  @override
+  String get filter_bedrooms => 'Yatak Odası';
+
+  @override
+  String get filter_bathrooms => 'Banyo';
+
+  @override
+  String get filter_any => 'Tümü';
+
+  @override
+  String get filter_apply_button => 'Sonuçları Göster';
+
+  @override
+  String get type_apartment => 'Daire';
+
+  @override
+  String get type_villa => 'Villa';
+
+  @override
+  String get type_farm => 'Çiftlik';
+
+  @override
+  String get type_country_house => 'Kır Evi';
+
+  @override
+  String get type_studio => 'Stüdyo';
 }
